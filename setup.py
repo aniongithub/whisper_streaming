0f4082884ec4d061879c3d528ef54a113c198bf0
aniongithub <ani@anionline.me>
2025-03-22 22:47:32 -0700

    Update setup,py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='whisper_streaming',
+    name='whisper-streaming',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
@@ -9,8 +9,8 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'whisper_online=whisper_streaming.whisper_online:__main__',
-            'whisper_online_server=whisper_streaming.whisper_online_server:__main__',
+            'whisper_online=whisper-streaming.whisper_online:__main__',
+            'whisper_online_server=whisper-streaming.whisper_online_server:__main__',
         ],
     },
 )
